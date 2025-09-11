e075485d4f4959ec9385ae9c968f05ab42c9bf4f
张飞亚 <2960885715@qq.com>
2025-09-11 16:18:50 +0800

    AI-Super-Agent : feat : 项目启动

---
+++ src/main/java/com/zfyedu/aitoursuperintelligentagent/AiTourSuperIntelligentAgentApplication.java
@@ -0,0 +1,14 @@
+package com.zfyedu.aitoursuperintelligentagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTourSuperIntelligentAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiTourSuperIntelligentAgentApplication.class, args);
+        System.out.println("AI旅游超级智能体启动启动>>>>>");
+    }
+
+}
---
+++ src/test/java/com/zfyedu/aitoursuperintelligentagent/AiTourSuperIntelligentAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zfyedu.aitoursuperintelligentagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTourSuperIntelligentAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
