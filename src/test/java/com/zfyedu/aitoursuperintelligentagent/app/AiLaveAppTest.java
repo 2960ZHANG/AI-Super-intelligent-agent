78c461ad466c7f362821aeff3321460a45d0e070
张飞亚 <2960885715@qq.com>
2025-09-15 14:27:23 +0800

    AI-Super-Agent : feat : 多轮对话

---
+++ src/test/java/com/zfyedu/aitoursuperintelligentagent/app/AiLaveAppTest.java
@@ -0,0 +1,31 @@
+package com.zfyedu.aitoursuperintelligentagent.app;
+
+import cn.hutool.core.lang.UUID;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLaveAppTest {
+@Resource
+private LoveApp loveApp;
+    @Test
+    void doChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是程序员鱼皮";
+        String answer = loveApp.doChat(message, chatId);
+        System.out.println(answer);
+        // 第二轮
+        message = "我的另一半叫xiaox";
+        answer = loveApp.doChat(message, chatId);
+        System.out.println(answer);
+        Assertions.assertNotNull(answer);
+        // 第三轮
+        message = "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
+        answer = loveApp.doChat(message, chatId);
+        System.out.println(answer);
+        Assertions.assertNotNull(answer);
+    }
+}
