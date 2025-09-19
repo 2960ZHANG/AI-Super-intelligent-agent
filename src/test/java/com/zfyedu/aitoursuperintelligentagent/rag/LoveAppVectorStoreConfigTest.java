c5abe76d7c83bc58eaad48a81212b79e25720cd7
张飞亚 <2960885715@qq.com>
2025-09-19 15:00:20 +0800

    AI-Super-Agent : feat : 基于内存的向量数据库 SimpleVectorStore

---
+++ src/test/java/com/zfyedu/aitoursuperintelligentagent/rag/LoveAppVectorStoreConfigTest.java
@@ -0,0 +1,25 @@
+package com.zfyedu.aitoursuperintelligentagent.rag;
+
+import com.zfyedu.aitoursuperintelligentagent.app.LoveApp;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class LoveAppVectorStoreConfigTest {
+
+    @Resource
+    LoveAppVectorStoreConfig loveAppVectorStoreConfig;
+    @Resource
+    EmbeddingModel deshSopeEmbeddingModel;
+
+    @Test
+    void loveAppVectorStore() {
+        VectorStore vectorStore = loveAppVectorStoreConfig.loveAppVectorStore(deshSopeEmbeddingModel);
+        assertNotNull(vectorStore);
+    }
+}
